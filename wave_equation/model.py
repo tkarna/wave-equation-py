80cac85dbea939c1f3b70f15a6b6fa3cb4eefd87
Tuomas Karna <tuomas.karna@intel.com>
2023-02-14 08:35:54 +0200

    wave eq: refactor; add test.py

--- wave_equation/model.py
+++ wave_equation/model.py
@@ -2,33 +2,7 @@ import numpy
 import matplotlib.pyplot as plt
 import math
 import time as time_mod
-
-# constants
-g = 9.81
-h = 1.0
-
-
-def exact_elev(grid, t):
-    """
-    Exact solution for elevation field.
-
-    Returns time-dependent elevation of a 2D standing wave in a rectangular
-    domain.
-    """
-    amp = 0.5
-    c = math.sqrt(g * h)
-    n = 1
-    sol_x = numpy.sin(2 * n * numpy.pi * grid.x_t_2d / grid.lx)
-    m = 1
-    sol_y = numpy.sin(2 * m * numpy.pi * grid.y_t_2d / grid.ly)
-    omega = c * numpy.pi * math.sqrt((n/grid.lx)**2 + (m/grid.ly)**2)
-    sol_t = numpy.cos(2 * omega * t)
-    return amp * sol_x * sol_y * sol_t
-
-
-def initial_elev(grid):
-    """Set initial condition for water elevation"""
-    return exact_elev(grid, 0)
+import constant
 
 
 class CGrid:
@@ -87,11 +61,14 @@ class CGrid:
         print(f'Total     DOFs: {self.dofs_T + self.dofs_U + self.dofs_V}')
 
 
-def model(nx, ny, initial_elev_func, exact_elev_func=None,
-          t_end=1.0, t_export=0.02, runtime_plot=False):
+def run(nx, ny, initial_elev_func, exact_elev_func=None,
+        t_end=1.0, t_export=0.02, runtime_plot=False):
     """
     Run simulation.
     """
+    g = constant.g
+    h = constant.h
+
     grid = CGrid(nx, ny)
 
     # state variables
@@ -219,13 +196,3 @@ def model(nx, ny, initial_elev_func, exact_elev_func=None,
         ax2.set_title('Exact')
 
         plt.show()
-
-
-if __name__ == '__main__':
-    n = 128
-    model(
-        n, n,
-        initial_elev_func=initial_elev,
-        exact_elev_func=exact_elev,
-        runtime_plot=True
-    )
---
+++ wave_equation/test.py
@@ -0,0 +1,37 @@
+import model
+import numpy
+import math
+import constant
+
+
+def exact_elev(grid, t):
+    """
+    Exact solution for elevation field.
+
+    Returns time-dependent elevation of a 2D standing wave in a rectangular
+    domain.
+    """
+    amp = 0.5
+    c = math.sqrt(constant.g * constant.h)
+    n = 1
+    sol_x = numpy.sin(2 * n * numpy.pi * grid.x_t_2d / grid.lx)
+    m = 1
+    sol_y = numpy.sin(2 * m * numpy.pi * grid.y_t_2d / grid.ly)
+    omega = c * numpy.pi * math.sqrt((n/grid.lx)**2 + (m/grid.ly)**2)
+    sol_t = numpy.cos(2 * omega * t)
+    return amp * sol_x * sol_y * sol_t
+
+
+def initial_elev(grid):
+    """Set initial condition for water elevation"""
+    return exact_elev(grid, 0)
+
+
+if __name__ == '__main__':
+    n = 128
+    model.run(
+        n, n,
+        initial_elev_func=initial_elev,
+        exact_elev_func=exact_elev,
+        runtime_plot=True
+    )
