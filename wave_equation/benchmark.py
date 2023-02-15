935ed16bc2bdc8e6ad10c804973be7d598b76c6b
Tuomas Karna <tuomas.karna@intel.com>
2023-02-15 21:30:42 +0200

    wave eq: add -p option to benchmark; times are milliseconds

--- wave_equation/benchmark.py
+++ wave_equation/benchmark.py
@@ -16,8 +16,12 @@ from test import initial_elev, exact_elev
               multiple=True,
               type=click.IntRange(min=4, max_open=True), show_default=True,
               help='Number of grid cells in x and y direction.')
+@click.option('-p', '--generate-plot', is_flag=True, default=False,
+              type=click.BOOL, show_default=True,
+              help='Generate a plot of timings.')
 def main(**kwargs):
     backend_list = kwargs['backend']
+    generate_plot = kwargs['generate_plot']
     # numpy needs to run before numba
     ordered_list = ['numpy', 'numba', 'ramba', 'jax']
     backend_list = [b for b in ordered_list if b in backend_list]
@@ -37,15 +41,7 @@ def main(**kwargs):
                 dt=dt, ntimestep=ntimestep, t_export=t_export,
                 backend=b,
             )
-            timings[(b, r)] = t/ntimestep
-
-    # compare against numpy
-    speedup = {}
-    for b in backend_list:
-        for r in reso_list:
-            t = timings[(b, r)]
-            t_ref = timings[('numpy', r)]
-            speedup[(b, r)] = t_ref/t
+            timings[(b, r)] = t/ntimestep*1000.
 
     dofs_list = [r*r + 2 * r*(r+1) for r in reso_list]
 
@@ -53,44 +49,60 @@ def main(**kwargs):
     backend_cols = ' '.join(f'{b:8s}' for b in backend_list)
     print(f'size dofs     {backend_cols}')
     for r, dofs in zip(reso_list, dofs_list):
-        time_cols = ' '.join(f'{timings[(b, r)]:8.2e}'
+        time_cols = ' '.join(f'{timings[(b, r)]:8.4g}'
                              for b in backend_list)
         print(f'{r:4d} {dofs:8d} {time_cols}')
 
-    # make a plot
-    import matplotlib.pyplot as plt
-    fig, ax_list = plt.subplots(nrows=1, ncols=2, figsize=(13, 5))
-    fig.suptitle('Linear wave equation')
-    ax = ax_list[0]
-    x = dofs_list
-    for b in backend_list:
-        y = [timings[(b, r)] for r in reso_list]
-        ax.loglog(x, y, label=b, marker='o')
-        if b == 'numpy':
-            for xi, yi, ri in zip(x, y, reso_list):
-                ax.text(xi, yi, f'{ri}x{ri}',
-                        va='top', ha='left', size='small')
-    ax.set_xlabel('Problem size (degrees of freedom)')
-    ax.set_ylabel('Time per time step (s)')
-    ax.grid(True, which='both')
-    ax.legend()
+    if generate_plot:
+        # make a plot
+        import matplotlib.pyplot as plt
 
-    ax = ax_list[1]
-    for b in backend_list:
-        y = [speedup[(b, r)] for r in reso_list]
-        ax.semilogx(x, y, label=b, marker='o')
-    ax.set_xlabel('Problem size (degrees of freedom)')
-    ax.set_ylabel('Speed-up vs. numpy')
-    ax.grid(True, which='both')
-    ax.legend()
+        plot_speedup = 'numpy' in backend_list
+        if plot_speedup:
+            # compare against numpy
+            speedup = {}
+            for b in backend_list:
+                for r in reso_list:
+                    t = timings[(b, r)]
+                    t_ref = timings[('numpy', r)]
+                    speedup[(b, r)] = t_ref/t
+        nplots = 2 if plot_speedup else 1
+        fig, ax_list = plt.subplots(nrows=1, ncols=nplots,
+                                    figsize=(6.5*nplots, 5),
+                                    squeeze=False)
 
-    # save figure
-    b_str = '-'.join(backend_list)
-    r_str = '-'.join(map(str, reso_list))
-    img_name = f'waveeq_{b_str}_{r_str}.png'
-    print(f'Saving {img_name}')
-    fig.savefig(img_name, bbox_inches='tight', dpi=200)
-    plt.close(fig)
+        fig.suptitle('Linear wave equation')
+        ax = ax_list[0, 0]
+        x = dofs_list
+        for b in backend_list:
+            y = [timings[(b, r)]/1000. for r in reso_list]
+            ax.loglog(x, y, label=b, marker='o')
+        for xi, ri in zip(x, reso_list):
+            t_min = min([timings[(b, ri)]/1000. for b in backend_list])
+            ax.text(xi, t_min, f'{ri}x{ri}',
+                    va='top', ha='left', size='small')
+        ax.set_xlabel('Problem size (degrees of freedom)')
+        ax.set_ylabel('Time per time step (s)')
+        ax.grid(True, which='both')
+        ax.legend()
+
+        if plot_speedup:
+            ax = ax_list[0, 1]
+            for b in backend_list:
+                y = [speedup[(b, r)] for r in reso_list]
+                ax.semilogx(x, y, label=b, marker='o')
+            ax.set_xlabel('Problem size (degrees of freedom)')
+            ax.set_ylabel('Speed-up vs. numpy')
+            ax.grid(True, which='both')
+            ax.legend()
+
+        # save figure
+        b_str = '-'.join(backend_list)
+        r_str = '-'.join(map(str, reso_list))
+        img_name = f'waveeq_{b_str}_{r_str}.png'
+        print(f'Saving {img_name}')
+        fig.savefig(img_name, bbox_inches='tight', dpi=200)
+        plt.close(fig)
 
 
 if __name__ == '__main__':
